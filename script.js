91b657fb2e130d3b17520d244d254a77bd636f49
Corse2B <tsilvain@lfitokyo.org>
2025-12-14 09:09:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const contents = document.querySelectorAll(".content");
 const overlay = document.getElementById("overlay");
 
 contents.forEach(content => {
+  const closeBtn = content.querySelector(".close-btn");
+
   content.addEventListener("click", () => {
     content.classList.add("active");
     overlay.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", (e) => {
+    e.stopPropagation(); // empêche le clic de rouvrir
+    content.classList.remove("active");
+    overlay.style.display = "none";
   });
 });
 
