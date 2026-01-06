67c10749452713f9734a4914b44131b6e1f5ae2c
Corse2B <tsilvain@lfitokyo.org>
2026-01-06 11:52:42 +0900

    Update script6.js

--- script6.js
+++ script6.js
@@ -1,6 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
 const intro = document.getElementById("intro");
 intro.volume = 0.25;
 
 setTimeout(() => {
   intro.muted = false;
 }, 50);
+});
